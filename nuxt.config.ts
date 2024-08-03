8b29ce96fdf7991f8265c2fb3cd9d1ca20ec2819
Pwntus <pontus.edvard@gmail.com>
2024-08-03 12:10:08 +0300

    feat: add analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -103,7 +103,6 @@ export default defineNuxtConfig({
         }
       ],
       script: [
-        /*
         {
           async: true,
           src: `https://www.googletagmanager.com/gtag/js?id=${process.env.GTAG_ID}`
@@ -115,7 +114,6 @@ export default defineNuxtConfig({
             gtag('js', new Date());
             gtag('config', '${process.env.GTAG_ID}');`
         }
-        */
       ],
       bodyAttrs: {
         class: 'antialiased h-full min-h-screen relative'
